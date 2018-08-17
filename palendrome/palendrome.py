a5d7b0876bb5e1b842819bc49fe02512ecd0ae33
Geoffserv <gtraugott@gmail.com>
2018-08-17 18:30:53 -0500

    Added file and stdin support

--- palendrome/palendrome.py
+++ palendrome/palendrome.py
@@ -1,19 +1,24 @@
 # palendrome.py
 import click
 
-def is_acronym(possible_acronym):
-	possible_acronym_length = len(possible_acronym)
-	for index in range(possible_acronym_length//2):		# // rounds to floor
-		if possible_acronym[index] != possible_acronym[(possible_acronym_length-index)-1]:
+def is_palendrome(possible_palendrome):
+	possible_palendrome_length = len(possible_palendrome)
+	for index in range(possible_palendrome_length//2):		# // rounds to floor
+		if possible_palendrome[index] != possible_palendrome[(possible_palendrome_length-index)-1]:
 			return False
 	return True
 
 @click.command()
 @click.argument(
-	'possible_acronym',
+	'input_file',
+	type=click.File('rb'),
 )
-def main(possible_acronym):
-	print(is_acronym(possible_acronym))
+def main(input_file):
+	"""Prints true if the input is palendromatic
+
+	Either specify a filename or give stdin e.g. echo "blablalbalb" | ./virtualenv/palendrome/bin/python ./palendrome.py -
+	"""
+	click.echo(is_palendrome(input_file.read().strip()))
 
 if __name__ == "__main__":
 	main()
